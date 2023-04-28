4648ac69f852e2aa17aae12983d7af5af70fb178
openc0de <openc0de@hotmail.com>
2023-04-28 19:43:55 +0800

    add a main file

---
+++ source/required/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author openc0de (openc0de@hotmail.com)
+ * @brief main file
+ * @version 0.00.001
+ * @date Fri Apr 28 2023 19:43:17 +0800
+ * @since Fri Apr 28 2023 19:43:17 +0800
+ * @details first edition
+ *
+ * @copyright copyright ©2023 haihe. all rights reserved.
+ *
+ */
