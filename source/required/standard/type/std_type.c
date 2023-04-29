9a83a07ced589c6ca3e8d04c7aacb96645bfa4d6
openc0de <openc0de@hotmail.com>
2023-04-29 17:14:07 +0800

    add stardardized type definitions files

---
+++ source/required/standard/type/std_type.c
@@ -0,0 +1,18 @@
+/**
+ * @file      \source\required\standard\type\std_type.c
+ * @brief     stardardized type definitions file
+ * @author    openc0de (openc0de@hotmail.com)
+ * @since     Sat Apr 29 2023 17:14:01 +0800
+ *
+ * @name      openc0de (openc0de@hotmail.com)
+ * @date      Sat Apr 29 2023 17:14:01 +0800
+ * @version   0.00.001
+ * @details   first edition
+ *
+ * @link      https://github.com/openc0de/haihe
+ *
+ * @copyright copyright ©2023 by openc0de, all rights reserved.
+ */
+
+#include "std_type_public.h"
+#include "std_type_private.h"
