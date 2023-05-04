72678398b142cb4532e06336ac1b7d94337f3e26
openc0de <openc0de@hotmail.com>
2023-05-04 11:57:09 +0800

    add a new main function api

--- source/required/main.c
+++ source/required/main.c
@@ -5,11 +5,23 @@
  * @date      Fri Apr 28 2023 19:43:17 +0800
  *
  * @name      openc0de (openc0de@hotmail.com)
- * @date      Sat Apr 29 2023 14:44:52 +0800
- * @version   0.00.002
- * @details   change header comments style
+ * @date      Thu May 4 2023 11:57:45 +0800
+ * @version   0.00.003
+ * @details   add a new main function api
  *
  * @link      https://github.com/openc0de/haihe
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
  */
+
+#include "sys_public.h"
+
+/**
+ * @brief main function
+ *
+ * @return 0
+ */
+i32 main(void)
+{
+    return (0);
+} /* main */
