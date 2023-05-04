429612d2a93102d12f5fdae66b1e63befb69206d
openc0de <openc0de@hotmail.com>
2023-05-04 11:59:44 +0800

    update

---
+++ .history/source/required/main_20230504114811.c
@@ -0,0 +1,27 @@
+/**
+ * @file      \source\required\main.c
+ * @brief     main file
+ * @author    openc0de (openc0de@hotmail.com)
+ * @date      Fri Apr 28 2023 19:43:17 +0800
+ *
+ * @name      openc0de (openc0de@hotmail.com)
+ * @date      Sat Apr 29 2023 14:44:52 +0800
+ * @version   0.00.002
+ * @details   change header comments style
+ *
+ * @link      https://github.com/openc0de/haihe
+ *
+ * @copyright copyright ©2023 by openc0de, all rights reserved.
+ */
+
+#include "sys_public.h"
+
+/**
+ * @brief main function
+ *
+ * @return 0
+ */
+i32 main(void)
+{
+    return (0);
+} /* main */
---
+++ .history/source/required/standard/std_20230504114510.c
@@ -0,0 +1,18 @@
+/**
+ * @file      \source\required\standard\std.c
+ * @brief     standardlized file
+ * @author    openc0de (openc0de@hotmail.com)
+ * @since     Sat Apr 29 2023 17:25:13 +0800
+ *
+ * @name      openc0de (openc0de@hotmail.com)
+ * @date      Sat Apr 29 2023 17:25:13 +0800
+ * @version   0.00.001
+ * @details   first edition
+ *
+ * @link      https://github.com/openc0de/haihe
+ *
+ * @copyright copyright ©2023 by openc0de, all rights reserved.
+ */
+
+#include "std_public.h"
+#include "std_private.h"
