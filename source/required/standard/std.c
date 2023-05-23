b8b6a97a67741a9c3673e62d3987a5fd8ecea344
openc0de <openc0de@hotmail.com>
2023-05-23 20:55:54 +0800

    变更标准文件头注释

--- source/required/standard/std.c
+++ source/required/standard/std.c
@@ -1,17 +1,21 @@
 /**
- * @file      \source\required\standard\std.c
- * @brief     standardlized file
- * @author    openc0de (openc0de@hotmail.com)
- * @since     Sat Apr 29 2023 17:25:13 +0800
+ * @file std.c
+ * @brief 标准文件
+ * @author openc0de (openc0de@hotmail.com)
+ * @since Sat Apr 29 2023 17:25 +0800
  *
- * @name      openc0de (openc0de@hotmail.com)
- * @date      Sat Apr 29 2023 17:25:13 +0800
- * @version   0.00.001
- * @details   first edition
- *
- * @link      https://github.com/openc0de/haihe
+ * @name openc0de (openc0de@hotmail.com)
+ * @date Tue May 23 2023 20:55 +0800
+ * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |             date            |       by       |    comments
+ * ------------ | --------------------------- | -------------- | ---------------
+ *   0.00.001   | Tue May 23 2023 20:55 +0800 |    openc0de    | 初版
+ * -----------------------------------------------------------------------------
  */
 
 #include "std_public.h"
