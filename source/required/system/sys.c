bfaaef1eb1d29fbcdb6154246d6c9e3ad68a6691
openc0de <openc0de@hotmail.com>
2023-06-10 00:13:54 +0800

    update

--- source/required/system/sys.c
+++ source/required/system/sys.c
@@ -2,10 +2,10 @@
  * @file sys.c
  * @brief 系统文件
  * @author openc0de (openc0de@hotmail.com)
- * @since Sat Apr 29 2023 22:32:01 +0800
+ * @since Sat Apr 29 2023 22:32 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Tue May 23 2023 20:49 +0800
+ * @date Sat Jun 10 2023 00:13 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -14,7 +14,7 @@
  * -----------------------------------------------------------------------------
  *    version   |             date            |       by       |    comments
  * ------------ | --------------------------- | -------------- | ---------------
- *   0.00.001   | Tue May 23 2023 20:49 +0800 |    openc0de    | 初版
+ *   0.00.001   | Sat Apr 29 2023 22:32 +0800 |    openc0de    | 初版
  * -----------------------------------------------------------------------------
  */
 
