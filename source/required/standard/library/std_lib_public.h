5ef50bc80ce97a6adf41c8688791dd93bd9e2897
openc0de <openc0de@hotmail.com>
2023-06-10 13:36:45 +0800

    修改标准库文件

--- source/required/standard/library/std_lib_public.h
+++ source/required/standard/library/std_lib_public.h
@@ -5,7 +5,7 @@
  * @since Sat Jun 10 2023 12:54 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Sat Jun 10 2023 12:56 +0800
+ * @date Sat Jun 10 2023 13:36 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -103,7 +103,7 @@
 
     /*macro*********************************************************************/
 
-
+    #define NULL    ((void *)0)
 
     /*structure*****************************************************************/
 
