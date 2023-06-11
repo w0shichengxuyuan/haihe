12fe6bcb132a36c8ba5ad0c4e0dc909ff2ad3f42
openc0de <openc0de@hotmail.com>
2023-06-11 22:06:54 +0800

    修改标准类型转换文件

--- source/required/standard/swap/type/std_swap_type.c
+++ source/required/standard/swap/type/std_swap_type.c
@@ -5,7 +5,7 @@
  * @since Sat Jun 10 2023 22:22 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Sun Jun 11 2023 22:05 +0800
+ * @date Sun Jun 11 2023 22:06 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -463,19 +463,21 @@ state_e e_std_swap_type_u32_to_u64(const u32 *u32_p_in,  u8  u8_in_len,
     u8   u8_quotient  = 0,
          u8_remainder = 0;
     u32 *u32_p_buffer = NULL;
+    u64 *u64_p_buffer = NULL;
 
     u32_p_buffer = u32_p_in;
+    u64_p_buffer = u64_p_out;
     u8_quotient  = u8_in_len >> 1;
     u8_remainder = u8_in_len  & 1;
 
     for (u8 i = 0; u8_quotient > i; ++i)
     {
-        *u64_p_out  = 0;
+        *u64_p_buffer  = 0;
 
-        *u64_p_out  = *u32_p_buffer++ << 32;
-        *u64_p_out |= *u32_p_buffer++      ;
+        *u64_p_buffer  = *u32_p_buffer++ << 32;
+        *u64_p_buffer |= *u32_p_buffer++      ;
 
-        ++u64_p_out;
+        ++u64_p_buffer;
     }
 
     *u8_p_out_len = u8_quotient;
@@ -485,9 +487,9 @@ state_e e_std_swap_type_u32_to_u64(const u32 *u32_p_in,  u8  u8_in_len,
     *u8_p_out_len += 1;
     for (u8 i = 0; u8_remainder > i; ++i)
     {
-        *u64_p_out = 0;
+        *u64_p_buffer = 0;
 
-        *u64_p_out = *u32_p_buffer++ << ((1 - i) << 5);
+        *u64_p_buffer = *u32_p_buffer++ << ((1 - i) << 5);
     }
 
     return (SUCCESS);
