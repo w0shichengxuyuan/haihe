e474a4b99de635cfa602555ff0e8cbfd80e4d72a
openc0de <openc0de@hotmail.com>
2023-06-11 22:03:34 +0800

    修改标准类型转换文件

--- source/required/standard/swap/type/std_swap_type.c
+++ source/required/standard/swap/type/std_swap_type.c
@@ -5,7 +5,7 @@
  * @since Sat Jun 10 2023 22:22 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Sun Jun 11 2023 22:01 +0800
+ * @date Sun Jun 11 2023 22:03 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -73,7 +73,7 @@ state_e e_std_swap_type_u8_to_u16(const u8  *u8_p_in,   u8  u8_in_len,
     *u8_p_out_len += 1;
     for (u8 i = 0; u8_remainder > i; ++i)
     {
-        *u16_p_buffer  = 0;
+        *u16_p_buffer = 0;
 
         *u16_p_buffer = *u8_p_buffer++ << ((1 - i) << 3);
     }
@@ -168,28 +168,30 @@ state_e e_std_swap_type_u8_to_u64(const u8  *u8_p_in,   u8  u8_in_len,
     if (NULL == u8_p_out_len)
         return (FAILURE);
 
-    u8 *u8_p_buffer  = NULL,
-        u8_quotient  = 0,
-        u8_remainder = 0;
+    u8   u8_quotient  = 0,
+         u8_remainder = 0,
+        *u8_p_buffer  = NULL;
+    u64 *u64_p_buffer = NULL;
 
     u8_p_buffer  = u8_p_in;
+    u64_p_buffer = u64_p_out;
     u8_quotient  = u8_in_len >> 3;
     u8_remainder = u8_in_len  & 7;
 
     for (u8 i = 0; u8_quotient > i; ++i)
     {
-        *u64_p_out  = 0;
+        *u64_p_buffer  = 0;
 
-        *u64_p_out  = *u8_p_buffer++ << 56;
-        *u64_p_out |= *u8_p_buffer++ << 48;
-        *u64_p_out |= *u8_p_buffer++ << 40;
-        *u64_p_out |= *u8_p_buffer++ << 32;
-        *u64_p_out |= *u8_p_buffer++ << 24;
-        *u64_p_out |= *u8_p_buffer++ << 16;
-        *u64_p_out |= *u8_p_buffer++ <<  8;
-        *u64_p_out |= *u8_p_buffer++      ;
+        *u64_p_buffer  = *u8_p_buffer++ << 56;
+        *u64_p_buffer |= *u8_p_buffer++ << 48;
+        *u64_p_buffer |= *u8_p_buffer++ << 40;
+        *u64_p_buffer |= *u8_p_buffer++ << 32;
+        *u64_p_buffer |= *u8_p_buffer++ << 24;
+        *u64_p_buffer |= *u8_p_buffer++ << 16;
+        *u64_p_buffer |= *u8_p_buffer++ <<  8;
+        *u64_p_buffer |= *u8_p_buffer++      ;
 
-        ++u64_p_out;
+        ++u64_p_buffer;
     }
 
     *u8_p_out_len = u8_quotient;
@@ -199,9 +201,9 @@ state_e e_std_swap_type_u8_to_u64(const u8  *u8_p_in,   u8  u8_in_len,
     *u8_p_out_len += 1;
     for (u8 i = 0; u8_remainder > i; ++i)
     {
-        *u64_p_out  = 0;
+        *u64_p_buffer = 0;
 
-        *u64_p_out = *u8_p_buffer++ << ((7 - i) << 3);
+        *u64_p_buffer = *u8_p_buffer++ << ((7 - i) << 3);
     }
 
     return (SUCCESS);
