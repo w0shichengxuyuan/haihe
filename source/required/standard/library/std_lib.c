5c0f4840d1e11c6b15ab04cf75fe7b0ef36f94fc
openc0de <openc0de@hotmail.com>
2023-06-14 22:24:10 +0800

    修改标准库文件

--- source/required/standard/library/std_lib.c
+++ source/required/standard/library/std_lib.c
@@ -5,7 +5,7 @@
  * @since Sat Jun 10 2023 12:54 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Sat Jun 10 2023 12:55 +0800
+ * @date Wed Jun 14 2023 22:23 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -18,5 +18,5 @@
  * -----------------------------------------------------------------------------
  */
 
-#include "std_lib_public.h"
 #include "std_lib_private.h"
+#include "std_lib_public.h"
--- source/required/standard/library/std_lib_private.h
+++ source/required/standard/library/std_lib_private.h
@@ -5,7 +5,7 @@
  * @since Sat Jun 10 2023 12:54 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Sat Jun 10 2023 12:56 +0800
+ * @date Wed Jun 14 2023 22:23 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -45,7 +45,7 @@
 
         /*standard**************************************************************/
 
-
+        #include "std_type_public.h"
 
         /*system****************************************************************/
 
