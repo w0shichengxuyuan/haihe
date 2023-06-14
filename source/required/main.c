a780970d28ac9a2fa6f0200fa39b405c3b86af63
openc0de <openc0de@hotmail.com>
2023-06-14 22:22:57 +0800

    修改主文件

--- source/required/main.c
+++ source/required/main.c
@@ -1,11 +1,11 @@
 /**
  * @file main.c
- * @brief 主函数
+ * @brief 主文件
  * @author openc0de (openc0de@hotmail.com)
  * @since Sun Apr 30 2023 20:16:20 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Tue May 23 2023 01:38:34 +0800
+ * @date Wed Jun 14 2023 22:22 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -25,7 +25,7 @@
  *
  * @return 0
  */
-i32 main(void)
+i32 main(vd)
 {
     return (0);
 } /* main */
