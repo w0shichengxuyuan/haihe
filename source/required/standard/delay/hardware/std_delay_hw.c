9ddb15c64e892905125a79007ce011bd2988051f
openc0de <openc0de@hotmail.com>
2023-06-15 22:18:22 +0800

    增加标准硬件延时文件

---
+++ source/required/standard/delay/hardware/std_delay_hw.c
@@ -0,0 +1,22 @@
+/**
+ * @file std_delay_hw.c
+ * @brief 标准硬件延时文件
+ * @author openc0de (openc0de@hotmail.com)
+ * @since Thu Jun 15 2023 22:16 +0800
+ *
+ * @name openc0de (openc0de@hotmail.com)
+ * @date Thu Jun 15 2023 22:18 +0800
+ * @version 0.00.001
+ *
+ * @copyright copyright ©2023 by openc0de, all rights reserved.
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |             date            |       by       |    comments
+ * ------------ | --------------------------- | -------------- | ---------------
+ *   0.00.001   | Thu Jun 15 2023 22:16 +0800 |    openc0de    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "std_delay_hw_private.h"
+#include "std_delay_hw_public.h"
