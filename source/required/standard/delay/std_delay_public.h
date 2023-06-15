5f9a812391e5cfeb4cf898420f8832699651f73b
openc0de <openc0de@hotmail.com>
2023-06-15 22:19:45 +0800

    修改标准延时文件

--- source/required/standard/delay/std_delay_public.h
+++ source/required/standard/delay/std_delay_public.h
@@ -5,7 +5,7 @@
  * @since Sat Jun 10 2023 12:46 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Wed Jun 14 2023 22:19 +0800
+ * @date Thu Jun 15 2023 22:19 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -45,7 +45,8 @@
 
         /*standard**************************************************************/
 
-
+        #include "std_delay_hw_public.h"
+        #include "std_delay_sw_public.h"
 
         /*system****************************************************************/
 
