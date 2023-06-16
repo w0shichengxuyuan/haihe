ce807568db072af119b68747df90608cacf7106f
openc0de <openc0de@hotmail.com>
2023-06-16 23:35:30 +0800

    增加标准断言文件

--- source/required/standard/assert/std_assert_public.h
+++ source/required/standard/assert/std_assert_public.h
@@ -1,11 +1,11 @@
 /**
- * @file std_public.h
- * @brief 标准公有文件
+ * @file std_assert_public.h
+ * @brief 标准断言公有文件
  * @author openc0de (openc0de@hotmail.com)
- * @since Sat Apr 29 2023 17:25 +0800
+ * @since Fri Jun 16 2023 23:31 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Fri Jun 16 2023 23:34 +0800
+ * @date Fri Jun 16 2023 23:32 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -14,12 +14,12 @@
  * -----------------------------------------------------------------------------
  *    version   |             date            |       by       |    comments
  * ------------ | --------------------------- | -------------- | ---------------
- *   0.00.001   | Sat Apr 29 2023 17:25 +0800 |    openc0de    | 初版
+ *   0.00.001   | Fri Jun 16 2023 23:31 +0800 |    openc0de    | 初版
  * -----------------------------------------------------------------------------
  */
 
-#ifndef __STD_PUBLIC_H__
-#define __STD_PUBLIC_H__
+#ifndef __STD_ASSERT_PUBLIC_H__
+#define __STD_ASSERT_PUBLIC_H__
 
 /*header files******************************************************************/
 
@@ -45,14 +45,7 @@
 
         /*standard**************************************************************/
 
-        #include "std_assert_public.h"
-        #include "std_crypto_public.h"
-        #include "std_delay_public.h"
-        #include "std_lib_public.h"
-        #include "std_math_public.h"
-        #include "std_state_public.h"
-        #include "std_swap_public.h"
-        #include "std_type_public.h"
+
 
         /*system****************************************************************/
 
@@ -134,4 +127,4 @@
 
 
 
-#endif /* __STD_PUBLIC_H__ */
+#endif /* __STD_ASSERT_PUBLIC_H__ */
