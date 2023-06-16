cfb31f99d5c7f32ad7f917530032020959b4d4dc
openc0de <openc0de@hotmail.com>
2023-06-16 23:40:49 +0800

    增加标准字符文件

---
+++ source/required/standard/string/std_string.c
@@ -0,0 +1,22 @@
+/**
+ * @file std_string.c
+ * @brief 标准字符文件
+ * @author openc0de (openc0de@hotmail.com)
+ * @since Fri Jun 16 2023 23:38 +0800
+ *
+ * @name openc0de (openc0de@hotmail.com)
+ * @date Fri Jun 16 2023 23:39 +0800
+ * @version 0.00.001
+ *
+ * @copyright copyright ©2023 by openc0de, all rights reserved.
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |             date            |       by       |    comments
+ * ------------ | --------------------------- | -------------- | ---------------
+ *   0.00.001   | Fri Jun 16 2023 23:38 +0800 |    openc0de    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "std_string_private.h"
+#include "std_string_public.h"
