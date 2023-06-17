5772e6b26c67529978bc39d560eef4bae5c2f99a
openc0de <openc0de@hotmail.com>
2023-06-17 15:03:06 +0800

    增加标准滤波文件

--- source/required/standard/filter/std_filter_private.h
+++ source/required/standard/filter/std_filter_private.h
@@ -1,11 +1,11 @@
 /**
- * @file std_public.h
- * @brief 标准公有文件
+ * @file std_filter_private.h
+ * @brief 标准滤波私有文件
  * @author openc0de (openc0de@hotmail.com)
- * @since Sat Apr 29 2023 17:25 +0800
+ * @since Sat Jun 17 2023 14:58 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Sat Jun 17 2023 15:01 +0800
+ * @date Sat Jun 17 2023 15:00 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -14,12 +14,12 @@
  * -----------------------------------------------------------------------------
  *    version   |             date            |       by       |    comments
  * ------------ | --------------------------- | -------------- | ---------------
- *   0.00.001   | Sat Apr 29 2023 17:25 +0800 |    openc0de    | 初版
+ *   0.00.001   | Sat Jun 17 2023 14:58 +0800 |    openc0de    | 初版
  * -----------------------------------------------------------------------------
  */
 
-#ifndef __STD_PUBLIC_H__
-#define __STD_PUBLIC_H__
+#ifndef __STD_FILTER_PRIVATE_H__
+#define __STD_FILTER_PRIVATE_H__
 
 /*header files******************************************************************/
 
@@ -45,15 +45,7 @@
 
         /*standard**************************************************************/
 
-        #include "std_assert_public.h"
-        #include "std_crypto_public.h"
-        #include "std_delay_public.h"
-        #include "std_filter_public.h"
-        #include "std_math_public.h"
-        #include "std_state_public.h"
-        #include "std_string_public.h"
-        #include "std_swap_public.h"
-        #include "std_type_public.h"
+
 
         /*system****************************************************************/
 
@@ -131,8 +123,12 @@
 
 /*declarations******************************************************************/
 
+    /*variable******************************************************************/
+
+
+
     /*function******************************************************************/
 
 
 
-#endif /* __STD_PUBLIC_H__ */
+#endif /* __STD_FILTER_PRIVATE_H__ */
