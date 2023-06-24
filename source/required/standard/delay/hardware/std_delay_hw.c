c1f6b11b9abe055e8f57917fd3c2bd107c803943
openc0de <openc0de@hotmail.com>
2023-06-24 22:21:52 +0800

    修改标准硬件延时文件

--- source/required/standard/delay/hardware/std_delay_hw.c
+++ source/required/standard/delay/hardware/std_delay_hw.c
@@ -5,7 +5,7 @@
  * @since Thu Jun 15 2023 22:16 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Tue Jun 20 2023 23:06 +0800
+ * @date Sat Jun 24 2023 22:21 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -25,7 +25,7 @@
  * @brief 基本硬件延时
  *
  */
-void v_std_delay_hw_do_nothing(void)
+void v_std_hw_delay_do_nothing(void)
 {
     #if 0
         NOP();
@@ -42,14 +42,14 @@ void v_std_delay_hw_do_nothing(void)
     #if 0
         __nop();
     #endif
-} /* v_std_delay_hw_do_nothing */
+} /* v_std_hw_delay_do_nothing */
 
 /**
  * @brief 微秒硬件延时
  *
  * @param[in] u32_size - 大小
  */
-void v_std_delay_hw_us(u32 u32_size)
+void v_std_hw_delay_us(u32 u32_size)
 {
     if (0 == u32_size)
         return;
@@ -57,15 +57,15 @@ void v_std_delay_hw_us(u32 u32_size)
     if (E_DELAY_US_MAX_SIZE < u32_size)
         return;
 
-    v_std_delay_hw_do_nothing();
-} /* v_std_delay_hw_us */
+    v_std_hw_delay_do_nothing();
+} /* v_std_hw_delay_us */
 
 /**
  * @brief 毫秒硬件延时
  *
  * @param[in] u32_size - 大小
  */
-void v_std_delay_hw_ms(u32 u32_size)
+void v_std_hw_delay_ms(u32 u32_size)
 {
     if (0 == u32_size)
         return;
@@ -73,15 +73,15 @@ void v_std_delay_hw_ms(u32 u32_size)
     if (E_DELAY_MS_MAX_SIZE < u32_size)
         return;
 
-    v_std_delay_hw_do_nothing();
-} /* v_std_delay_hw_ms */
+    v_std_hw_delay_do_nothing();
+} /* v_std_hw_delay_ms */
 
 /**
  * @brief 秒硬件延时
  *
  * @param[in] u32_size - 大小
  */
-void v_std_delay_hw_s(u32 u32_size)
+void v_std_hw_delay_s(u32 u32_size)
 {
     if (0 == u32_size)
         return;
@@ -89,15 +89,15 @@ void v_std_delay_hw_s(u32 u32_size)
     if (E_DELAY_S_MAX_SIZE < u32_size)
         return;
 
-    v_std_delay_hw_do_nothing();
-} /* v_std_delay_hw_ms */
+    v_std_hw_delay_do_nothing();
+} /* v_std_hw_delay_ms */
 
 /**
  * @brief 分钟硬件延时
  *
  * @param[in] u32_size - 大小
  */
-void v_std_delay_hw_min(u32 u32_size)
+void v_std_hw_delay_min(u32 u32_size)
 {
     if (0 == u32_size)
         return;
@@ -105,185 +105,185 @@ void v_std_delay_hw_min(u32 u32_size)
     if (E_DELAY_MIN_MAX_SIZE < u32_size)
         return;
 
-    v_std_delay_hw_do_nothing();
-} /* v_std_delay_hw_min */
+    v_std_hw_delay_do_nothing();
+} /* v_std_hw_delay_min */
 
 /**
  * @brief 1微秒硬件延时
  *
  */
-void v_std_delay_hw_1us(void)
+void v_std_hw_delay_1us(void)
 {
-    v_std_delay_hw_us(1);
-} /* v_std_delay_hw_1us */
+    v_std_hw_delay_us(1);
+} /* v_std_hw_delay_1us */
 
 /**
  * @brief 10微秒硬件延时
  *
  */
-void v_std_delay_hw_10us(void)
+void v_std_hw_delay_10us(void)
 {
-    v_std_delay_hw_us(10);
-} /* v_std_delay_hw_10us */
+    v_std_hw_delay_us(10);
+} /* v_std_hw_delay_10us */
 
 /**
  * @brief 50微秒硬件延时
  *
  */
-void v_std_delay_hw_50us(void)
+void v_std_hw_delay_50us(void)
 {
-    v_std_delay_hw_us(50);
-} /* v_std_delay_hw_50us */
+    v_std_hw_delay_us(50);
+} /* v_std_hw_delay_50us */
 
 /**
  * @brief 100微秒硬件延时
  *
  */
-void v_std_delay_hw_100us(void)
+void v_std_hw_delay_100us(void)
 {
-    v_std_delay_hw_us(100);
-} /* v_std_delay_hw_100us */
+    v_std_hw_delay_us(100);
+} /* v_std_hw_delay_100us */
 
 /**
  * @brief 200微秒硬件延时
  *
  */
-void v_std_delay_hw_200us(void)
+void v_std_hw_delay_200us(void)
 {
-    v_std_delay_hw_us(200);
-} /* v_std_delay_hw_200us */
+    v_std_hw_delay_us(200);
+} /* v_std_hw_delay_200us */
 
 /**
  * @brief 500微秒硬件延时
  *
  */
-void v_std_delay_hw_500us(void)
+void v_std_hw_delay_500us(void)
 {
-    v_std_delay_hw_us(500);
-} /* v_std_delay_hw_500us */
+    v_std_hw_delay_us(500);
+} /* v_std_hw_delay_500us */
 
 /**
  * @brief 1毫秒硬件延时
  *
  */
-void v_std_delay_hw_1ms(void)
+void v_std_hw_delay_1ms(void)
 {
-    v_std_delay_hw_ms(1);
-} /* v_std_delay_hw_1ms */
+    v_std_hw_delay_ms(1);
+} /* v_std_hw_delay_1ms */
 
 /**
  * @brief 10毫秒硬件延时
  *
  */
-void v_std_delay_hw_10ms(void)
+void v_std_hw_delay_10ms(void)
 {
-    v_std_delay_hw_ms(10);
-} /* v_std_delay_hw_10ms */
+    v_std_hw_delay_ms(10);
+} /* v_std_hw_delay_10ms */
 
 /**
  * @brief 50毫秒硬件延时
  *
  */
-void v_std_delay_hw_50ms(void)
+void v_std_hw_delay_50ms(void)
 {
-    v_std_delay_hw_ms(50);
-} /* v_std_delay_hw_50ms */
+    v_std_hw_delay_ms(50);
+} /* v_std_hw_delay_50ms */
 
 /**
  * @brief 100毫秒硬件延时
  *
  */
-void v_std_delay_hw_100ms(void)
+void v_std_hw_delay_100ms(void)
 {
-    v_std_delay_hw_ms(100);
-} /* v_std_delay_hw_100ms */
+    v_std_hw_delay_ms(100);
+} /* v_std_hw_delay_100ms */
 
 /**
  * @brief 200毫秒硬件延时
  *
  */
-void v_std_delay_hw_200ms(void)
+void v_std_hw_delay_200ms(void)
 {
-    v_std_delay_hw_ms(200);
-} /* v_std_delay_hw_200ms */
+    v_std_hw_delay_ms(200);
+} /* v_std_hw_delay_200ms */
 
 /**
  * @brief 500毫秒硬件延时
  *
  */
-void v_std_delay_hw_500ms(void)
+void v_std_hw_delay_500ms(void)
 {
-    v_std_delay_hw_ms(500);
-} /* v_std_delay_hw_500ms */
+    v_std_hw_delay_ms(500);
+} /* v_std_hw_delay_500ms */
 
 /**
  * @brief 1秒硬件延时
  *
  */
-void v_std_delay_hw_1s(void)
+void v_std_hw_delay_1s(void)
 {
-    v_std_delay_hw_s(1);
-} /* v_std_delay_hw_1s */
+    v_std_hw_delay_s(1);
+} /* v_std_hw_delay_1s */
 
 /**
  * @brief 2秒硬件延时
  *
  */
-void v_std_delay_hw_2s(void)
+void v_std_hw_delay_2s(void)
 {
-    v_std_delay_hw_s(2);
-} /* v_std_delay_hw_2s */
+    v_std_hw_delay_s(2);
+} /* v_std_hw_delay_2s */
 
 /**
  * @brief 5秒硬件延时
  *
  */
-void v_std_delay_hw_5s(void)
+void v_std_hw_delay_5s(void)
 {
-    v_std_delay_hw_s(5);
-} /* v_std_delay_hw_5s */
+    v_std_hw_delay_s(5);
+} /* v_std_hw_delay_5s */
 
 /**
  * @brief 10秒硬件延时
  *
  */
-void v_std_delay_hw_10s(void)
+void v_std_hw_delay_10s(void)
 {
-    v_std_delay_hw_s(10);
-} /* v_std_delay_hw_10s */
+    v_std_hw_delay_s(10);
+} /* v_std_hw_delay_10s */
 
 /**
  * @brief 15秒硬件延时
  *
  */
-void v_std_delay_hw_15s(void)
+void v_std_hw_delay_15s(void)
 {
-    v_std_delay_hw_s(15);
-} /* v_std_delay_hw_15s */
+    v_std_hw_delay_s(15);
+} /* v_std_hw_delay_15s */
 
 /**
  * @brief 20秒硬件延时
  *
  */
-void v_std_delay_hw_20s(void)
+void v_std_hw_delay_20s(void)
 {
-    v_std_delay_hw_s(20);
-} /* v_std_delay_hw_20s */
+    v_std_hw_delay_s(20);
+} /* v_std_hw_delay_20s */
 
 /**
  * @brief 30秒硬件延时
  *
  */
-void v_std_delay_hw_30s(void)
+void v_std_hw_delay_30s(void)
 {
-    v_std_delay_hw_s(30);
-} /* v_std_delay_hw_30s */
+    v_std_hw_delay_s(30);
+} /* v_std_hw_delay_30s */
 
 /**
  * @brief 1分钟硬件延时
  *
  */
-void v_std_delay_hw_1min(void)
+void v_std_hw_delay_1min(void)
 {
-    v_std_delay_hw_min(1);
-} /* v_std_delay_hw_1min */
+    v_std_hw_delay_min(1);
+} /* v_std_hw_delay_1min */
