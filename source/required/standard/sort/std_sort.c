9ef0d258408aa4a5a87a1a26b8b2fd7886ed8b45
openc0de <openc0de@hotmail.com>
2023-06-24 22:48:38 +0800

    修改标准排序文件

--- source/required/standard/sort/std_sort.c
+++ source/required/standard/sort/std_sort.c
@@ -5,7 +5,7 @@
  * @since Sat Jun 17 2023 15:52 +0800
  *
  * @name openc0de (openc0de@hotmail.com)
- * @date Sat Jun 24 2023 22:40 +0800
+ * @date Sat Jun 24 2023 22:47 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by openc0de, all rights reserved.
@@ -119,3 +119,36 @@ void v_std_max_selection_sort(u8 *u8_p_data, u32 u32_length)
         u8_p_data[u32_max] = u8_tmp;
     }
 } /* v_std_max_selection_sort */
+
+/**
+ * @brief 插入排序
+ *
+ * @param u8_p_data  - 数据
+ * @param u32_length - 数据长度
+ */
+void v_std_insertion_sort(u8 *u8_p_data, u32 u32_length)
+{
+    v_std_assert_pointer(u8_p_data);
+    v_std_assert_min_invalid(0, u32_length);
+    v_std_assert_max_valid(M_SORT_MAX_SIZE, u32_length);
+
+    for (u32 i = 1; u32_length > i; ++i)
+    {
+        if (u8_p_data[i - 1] > u8_p_data[i])
+        {
+            u32 j = 0;
+            u8  u8_tmp = 0;
+
+            u8_tmp = u8_p_data[i];
+            u8_p_data[i] = u8_p_data[i - 1];
+
+            while (u8_p_data[j] > u8_tmp)
+            {
+                u8_p_data[j + 1] = u8_p_data[j];
+                --j;
+            }
+
+            u8_p_data[j + 1] = u8_tmp;
+        }
+    }
+} /* v_std_insertion_sort */
