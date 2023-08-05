e85f2e2f8b40638d7ed56b47b787412b781c8e0f
org.opencode <org.opencode@outlook.com>
2023-08-06 00:23:41 +0800

    更新

---
+++ tool/.history/haihe/source/required/main_20230806002313.c
@@ -0,0 +1,31 @@
+/**
+ * @file main.c
+ * @brief 主文件
+ * @author opencode (openc0de@hotmail.com)
+ * @since 2023-04-30 20:16 +0800
+ *
+ * @authors org.opencode (org.opencode@outlook.com)
+ * @date 2023-08-06 00:23 +0800
+ * @version 0.00.001
+ *
+ * @copyright copyright ©2023 by opencode, all rights reserved.
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |           date         |       by       |       comments
+ * ------------ | ---------------------- | -------------- | --------------------
+ *   0.00.001   | 2023-04-30 20:16 +0800 |  org.opencode  | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "sys_public.h"
+
+/**
+ * @brief 主函数
+ *
+ * @return 0
+ */
+i32 main(void)
+{
+    return (0);
+} /* main */
