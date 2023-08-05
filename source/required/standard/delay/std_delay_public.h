f9ecfa049e747ccac3d7121a32ec625014857aca
woshichengxuyuan <woshichengxuyuan@hotmail.com>
2023-08-05 12:31:41 +0800

    修改标准延时文件

--- source/required/standard/delay/std_delay_public.h
+++ source/required/standard/delay/std_delay_public.h
@@ -2,7 +2,7 @@
  * @file std_delay_public.h
  * @brief 标准延时公有文件
  * @author opencode (openc0de@hotmail.com)
- * @since Sat Jun 10 2023 12:46 +0800
+ * @since 2023-06-10 12:46 +0800
  *
  * @authors opencode (openc0de@hotmail.com)
  * @date 2023-08-04 23:30 +0800
