55ca09a458b7983ffe9afc36ab4b11f8e720bc64
woshichengxuyuan <woshichengxuyuan@hotmail.com>
2023-08-06 15:38:19 +0800

    修改标准转换文件

--- source/required/standard/swap/std_swap_private.h
+++ source/required/standard/swap/std_swap_private.h
@@ -2,19 +2,19 @@
  * @file std_swap_private.h
  * @brief 标准转换私有文件
  * @author opencode (openc0de@hotmail.com)
- * @since Sat Jun 10 2023 12:35 +0800
+ * @since 2023-08-06 13:03 +0800
  *
- * @authors opencode (openc0de@hotmail.com)
- * @date 2023-08-04 23:30 +0800
+ * @authors woshichengxuyuan (woshichengxuyuan@hotmail.com)
+ * @date 2023-08-06 15:37 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by opencode, all rights reserved.
  *
  * @details
  * -----------------------------------------------------------------------------
- *    version   |             date            |       by       |    comments
- * ------------ | --------------------------- | -------------- | ---------------
- *   0.00.001   | Sat Jun 10 2023 12:35 +0800 |    opencode    | 初版
+ *    version   |           date         |       by       |       comments
+ * ------------ | ---------------------- | -------------- | --------------------
+ *   0.00.001   | 2023-08-06 13:03 +0800 |    opencode    | 初版
  * -----------------------------------------------------------------------------
  */
 
