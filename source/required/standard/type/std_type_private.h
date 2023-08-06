8d7adb69e09a14be98ca2021bf7ea81460f7f1ba
woshichengxuyuan <woshichengxuyuan@hotmail.com>
2023-08-06 15:28:01 +0800

    修改标准类型文件

--- source/required/standard/type/std_type_private.h
+++ source/required/standard/type/std_type_private.h
@@ -2,19 +2,19 @@
  * @file std_type_private.h
  * @brief 标准类型私有文件
  * @author opencode (openc0de@hotmail.com)
- * @since Sat Apr 29 2023 17:14 +0800
+ * @since 2023-04-29 17:14 +0800
  *
- * @authors opencode (openc0de@hotmail.com)
- * @date 2023-08-04 23:30 +0800
+ * @authors woshichengxuyuan (woshichengxuyuan@hotmail.com)
+ * @date 2023-08-06 15:22 +0800
  * @version 0.00.001
  *
  * @copyright copyright ©2023 by opencode, all rights reserved.
  *
  * @details
  * -----------------------------------------------------------------------------
- *    version   |             date            |       by       |    comments
- * ------------ | --------------------------- | -------------- | ---------------
- *   0.00.001   | Sat Apr 29 2023 17:14 +0800 |    opencode    | 初版
+ *    version   |           date         |       by       |       comments
+ * ------------ | ---------------------- | -------------- | --------------------
+ *   0.00.001   | 2023-04-29 17:14 +0800 |    opencode    | 初版
  * -----------------------------------------------------------------------------
  */
 
