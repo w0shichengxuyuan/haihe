2124813621b76ce6e6433c379480ec4731c0a0bf
woshichengxuyuan <woshichengxuyuan@hotmail.com>
2023-08-06 15:13:56 +0800

    更新

---
+++ tool/.history/haihe/source/required/standard/type/std_type_20230806151321.c
@@ -0,0 +1,42 @@
+/**
+ * @file std_type.c
+ * @brief 标准类型文件
+ * @author opencode (openc0de@hotmail.com)
+ * @since 2023-04-29 17:14 +0800
+ *
+ * @authors woshichengxuyuan (woshichengxuyuan@hotmail.com)
+ * @date 2023-08-06 15:13 +0800
+ * @version 0.00.001
+ *
+ * @copyright copyright ©2023 by opencode, all rights reserved.
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |           date         |       by       |       comments
+ * ------------ | ---------------------- | -------------- | --------------------
+ *   0.00.001   | 2023-04-29 17:14 +0800 |    opencode    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ * @file std_type.c
+ * @brief 标准类型文件
+ * @author opencode (openc0de@hotmail.com)
+ * @since 2023-04-29 17:14 +0800
+ *
+ * @authors woshichengxuyuan (woshichengxuyuan@hotmail.com)
+ * @date 2023-08-04 23:30 +0800
+ * @version 0.00.001
+ *
+ * @copyright copyright ©2023 by opencode, all rights reserved.
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |             date            |       by       |    comments
+ * ------------ | --------------------------- | -------------- | ---------------
+ *   0.00.001   | Sat Apr 29 2023 17:14 +0800 |    opencode    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "std_type_public.h"
+#include "std_type_private.h"
