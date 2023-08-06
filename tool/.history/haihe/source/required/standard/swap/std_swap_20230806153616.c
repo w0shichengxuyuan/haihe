59c533c79b2219177ec2b9f4e6530f1143572e4f
woshichengxuyuan <woshichengxuyuan@hotmail.com>
2023-08-06 15:41:30 +0800

    更新

---
+++ tool/.history/haihe/source/required/standard/swap/std_swap_20230806153616.c
@@ -0,0 +1,22 @@
+/**
+ * @file std_swap_endian.c
+ * @brief 标准转换文件
+ * @author opencode (openc0de@hotmail.com)
+ * @since 2023-06-10 12:35 +0800
+ *
+ * @authors woshichengxuyuan (woshichengxuyuan@hotmail.com)
+ * @date 2023-08-06 15:36 +0800
+ * @version 0.00.001
+ *
+ * @copyright copyright ©2023 by opencode, all rights reserved.
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |             date            |       by       |    comments
+ * ------------ | --------------------------- | -------------- | ---------------
+ *   0.00.001   | Sat Jun 10 2023 12:35 +0800 |    opencode    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "std_swap_public.h"
+#include "std_swap_private.h"
---
+++ tool/.history/haihe/source/required/standard/swap/std_swap_20230806153650.c
@@ -0,0 +1,42 @@
+/**
+ * @file std_swap.c
+ * @brief std_swap
+ * @author woshichengxuyuan (woshichengxuyuan@hotmail.com)
+ * @since 2023-08-06 13:03 +0800
+ *
+ * @authors woshichengxuyuan (woshichengxuyuan@hotmail.com)
+ * @date 2023-08-06 15:36 +0800
+ * @version 0.00.001
+ *
+ * @copyright copyright ©2023 by woshichengxuyuan, all rights reserved.
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |           date         |       by       |       comments
+ * ------------ | ---------------------- | -------------- | --------------------
+ *   0.00.001   | 2023-08-06 15:36 +0800 |woshichengxuyuan| 初版
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ * @file std_swap_endian.c
+ * @brief 标准转换文件
+ * @author opencode (openc0de@hotmail.com)
+ * @since 2023-08-06 13:03 +0800
+ *
+ * @authors woshichengxuyuan (woshichengxuyuan@hotmail.com)
+ * @date 2023-08-06 15:36 +0800
+ * @version 0.00.001
+ *
+ * @copyright copyright ©2023 by opencode, all rights reserved.
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |           date         |       by       |       comments
+ * ------------ | ---------------------- | -------------- | --------------------
+ *   0.00.001   | 2023-08-06 13:03 +0800 |    opencode    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "std_swap_public.h"
+#include "std_swap_private.h"
