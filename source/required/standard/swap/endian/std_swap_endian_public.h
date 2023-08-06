5904d45e353f4d1a2dc3453333b1cc49db0ae01b
woshichengxuyuan <woshichengxuyuan@hotmail.com>
2023-08-06 16:14:18 +0800

    修改标准字节序转换文件

--- source/required/standard/swap/endian/std_swap_endian_public.h
+++ source/required/standard/swap/endian/std_swap_endian_public.h
@@ -1,20 +1,20 @@
 /**
  * @file std_swap_endian_public.h
  * @brief 标准字节序转换公有文件
- * @author opencode (openc0de@hotmail.com)
- * @since Sat Jun 10 2023 12:29 +0800
+ * @author org.opencode (org.opencode@outlook.com)
+ * @since 2023-08-06 13:03 +0800
  *
- * @authors opencode (openc0de@hotmail.com)
- * @date 2023-08-04 23:30 +0800
+ * @authors woshichengxuyuan (woshichengxuyuan@hotmail.com)
+ * @date 2023-08-06 16:13 +0800
  * @version 0.00.001
  *
- * @copyright copyright ©2023 by opencode, all rights reserved.
+ * @copyright copyright ©2023 by org.opencode, all rights reserved.
  *
  * @details
  * -----------------------------------------------------------------------------
- *    version   |             date            |       by       |    comments
- * ------------ | --------------------------- | -------------- | ---------------
- *   0.00.001   | Sat Jun 10 2023 12:29 +0800 |    opencode    | 初版
+ *    version   |           date         |       by       |       comments
+ * ------------ | ---------------------- | -------------- | --------------------
+ *   0.00.001   | 2023-08-06 16:12 +0800 |  org.opencode  | 初版
  * -----------------------------------------------------------------------------
  */
 
